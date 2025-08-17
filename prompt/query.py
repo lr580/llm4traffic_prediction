54c313ecdbc7fa0c84a6ec51a3d138306c2f73bc
lr580 <2277379149@qq.com>
2025-08-17 08:01:05 +0000

    实现了Ollama API查询

--- prompt/query.py
+++ prompt/query.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 import json
+import requests
 class CacheQuery():
     '''如果没有询问过，就调用API询问，如果询问过，返回询问结果。\n
     无上下文，不使用流。'''
@@ -30,22 +31,25 @@ class CacheQuery():
         content = self.makeQuery(filepath, message)
         return content
                 
-    def makeQuery(self, filepath:str, message:dict)->str:
+    def makeQuery(self, filepath:str, message:list)->str:
         response = self.client.chat.completions.create(
             model=self.model,
             messages=message,
+            stream=False,
         )
         with open(filepath, 'w', encoding='utf-8') as f:
             result = response.model_dump_json(indent=2)
             f.write(result)
         content = response.choices[0].message.content
         return content
+
+    def extractCache(self, data:dict)->str:
+        return '' # to be implememted by subclass
     
     def readCache(self, filepath:str)->str:
         with open(filepath, encoding='utf-8') as f:
             data = json.load(f)
-        content = data['choices'][0]['message']['content']
-        return content
+        return self.extractCache(data)
     
 with open(r'prompt/deepseek_apikey.txt') as f:
     deepseek_apikey = f.read().strip()
@@ -58,7 +62,36 @@ class DeepseekQuery(CacheQuery):
         self.client = OpenAI(api_key=self.apikey, base_url="https://api.deepseek.com")
         self.model = model 
         """ 'deepseek-chat' or 'deepseek-reasoner' """
+
+    def extractCache(self, data:dict)->str:
+        return data['choices'][0]['message']['content']
+
+class OllamaQuery(CacheQuery):
+    def __init__(self, path='', url='http://127.0.0.1:11434/api/chat', model='deepseek-r1:7b'):
+        super().__init__(path)
+        # self.client = OpenAI(base_url=url, api_key='None')
+        self.model = model
+        self.url = url
+
+    def makeQuery(self, filepath:str, message:list)->str:
+        payload = {
+            "model" : self.model,
+            "messages": message,
+            "stream": False
+        }
+        response = requests.post(self.url, json=payload)
+        response.raise_for_status()
+        result = response.json()
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=2)
+        return self.extractCache(result)
+
+    def extractCache(self, data:dict)->str:
+        return data['message']['content']
         
 if __name__ == "__main__":
-    q = DeepseekQuery('results/testClass')
-    print(q.query('test001-B', '中间件开发和后端有何区别'))
+    # q = DeepseekQuery('results/testClass')
+    # print(q.query('test001-C', '前端和后端有何区别'))
+    q = OllamaQuery('results/testClass')
+    # print(q.query('test001-E', '三维前缀和差分的公式是？'))
+    print(q.query('test001-G', '我刚刚询问了什么问题')) # 默认无上下文
